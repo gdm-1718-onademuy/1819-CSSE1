bb801adbe9ec5dfd8cffe2eef74468c49ae90c4b
Ona Demuytere <onademuy@student.arteveldehs.be>
2018-10-10 11:58:14 +0200

    klaar

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1819_CSSE1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        int a = 5;
+        int b = 6;
+        int resultaat1 = optellen(int a, int b);
+        int resultaat2 = deling(int a, int b);
+        int resultaat3 = verhogen(int a);
+
+        static int optellen(int a, int b)
+        {
+            return a + b;
+
+        }
+
+        static int verhogen(int a)
+        {
+            return a++;
+        }
+
+        static int deling (int a, int b)
+        {
+            return a/b;
+        }
+    }
+}
