9b79a3291a8814e10c9a3d75d32721b876a57072
Ona Demuytere <onademuy@student.arteveldehs.be>
2018-12-22 12:06:11 +0100

    coffee

--- opdrachten/opdracht6/deel4/program.cs
+++ opdrachten/opdracht6/deel4/program.cs
@@ -1,110 +1,322 @@
 using System;
+using System.Collections.Generic;
 
-namespace coffeeTypes
+namespace coffeeBuilder
 {
     class Program 
     {
         static void Main(string[] args)
         {
-            var var productCreator1 = new ProductCreator(new EspressoBuilder());
-            productCreator1.CreateProduct();
-            var product1 = productCreator1.GetProduct();
-            product1.ShowInfo();
-            
-            Console.WriteLine("---------------------------------------------");
-            
-            var productCreator2 = new ProductCreator(new MachiattoBuilder());
-            productCreator2.CreateProduct();
-            var product2 = productCreator2.GetProduct();
-            product2.ShowInfo();
+            CoffeeBuilder builder;
+            //create coffee with coffeebuilder
+            Coffee coffee = new Coffee();
 
+            //construct the coffees
+            builder = new EspressoBuilder();
+            coffee.Construct(builder);
+            builder.Coffee.Show();
+
+            builder = new MacchiatoBuilder();
+            coffee.Construct(builder);
+            builder.Coffee.Show();
+
+            builder = new RistrettoBuilder();
+            coffee.Construct(builder);
+            builder.Coffee.Show();
+
+            builder = new MochaBuilder();
+            coffee.Construct(builder);
+            builder.Coffee.Show();
+
+            builder = new IrishBuilder();
+            coffee.Construct(builder);
+            builder.Coffee.Show();
+
+            builder = new FrappeBuilder();
+            coffee.Construct(builder);
+            builder.Coffee.Show();
+
+            builder = new LatteBuilder();
+            coffee.Construct(builder);
+            builder.Coffee.Show();
+
+            builder = new GlaceBuilder();
+            coffee.Construct(builder);
+            builder.Coffee.Show();
+
+            builder = new AmericanoBuilder();
+            coffee.Construct(builder);
+            builder.Coffee.Show();
+
+            builder = new CappuccinoBuilder();
+            coffee.Construct(builder);
+            builder.Coffee.Show();
+            
+
+            //wait for user
             Console.ReadKey();
         }
     }
-    public interface IProductBuilder{
-        void SetSoort();
-        void SetStap1();
-        void SetStap2();
-        void SetStap3();
-        void SetStap4();
-        Product GetProduct();
-    }
-    public class EspressoBuilder : IProductBuilder{
-        Product objProduct = new Product();
-        public void SetSoort(){
-            objProduct.soort = "Espresso";
-        }
-        public void SetStap1(){
-            objProduct.stap1 = "Voeg Melk toe";
-        }
-        public void SetStap2() {
-            objProduct.stap2 = "Klop Melk schuim";
-        }
-        public void SetStap3() {
-            objProduct.stap3 = "Voeg espresso toe";
-        }
-        public void SetStap4() {
-            objProduct.stap4 = "Serveer";
-        }
-        public Product GetProduct(){
-            return objProduct;
+    //The director class
+    class Coffee
+    {
+        public void Construct(CoffeeBuilder coffeeBuilder)
+        {
+            coffeeBuilder.BuildTaste();
+            coffeeBuilder.BuildMilk();
+            coffeeBuilder.BuildSize();
         }
     }
-        public class MachiattoBuilder : IProductBuilder{
-            Product objProduct = new Product();
-            public void SetSoort(){
-                objProduct.soort = "Machiatto";
-            }
-            public void SetStap1(){
-                objProduct.stap1 = "Voeg Melk toe";
-            }
-            public void SetStap2() {
-                objProduct.stap2 = "Klop Melk schuim";
-            }
-            public void SetStap3() {
-                objProduct.stap3 = "Voeg Machiatto toe";
-            }
-            public void SetStap4() {
-                objProduct.stap4 = "Serveer";
-            }
-            public Product GetProduct(){
-             return objProduct;
-            }
+    abstract class CoffeeBuilder
+    {
+        protected Coffee coffee;
+
+        public Coffee Coffee
+        {
+            get {return coffee; }
         }
-        public class Product{
-            public string soort { get; set; }
-            public string stap1 { get; set; }
-            public string stap2 { get; set; }
-            public string stap3 { get; set; }
-            public string stap4 { get; set; }
-            public Product()
-            {
-             
-            }
-            public void ShowInfo(){
-                Console.WriteLine("Soort: {0}", soort);
-                Console.WriteLine("Stap1: {0}", stap1);
-                Console.WriteLine("Stap2: {0}", stap2);
-                Console.WriteLine("Stap3: {0}", stap3);
-                Console.WriteLine("Stap4: {0}", stap4);
-            }
+
+        public abstract void BuildTaste();
+        public abstract void BuildMilk();
+        public abstract void BuildSize();
+    }
+
+    //ESPRESSO
+    class EspressoBuilder : CoffeeBuilder
+    {
+        public EspressoBuilder()
+        {
+            coffee = new Coffee("Espresso");
         }
-            public class ProductCreator{
-            private readonly IProductBuilder objBuilder;
-            
-            public ProductCreator(IProductBuilder builder){
-                objBuilder = builder;
-            }
-            
-            public void CreateProduct(){
-                objBuilder.SetSoort();
-                objBuilder.SetStap1();
-                objBuilder.SetStap2();
-                objBuilder.SetStap3();
-                objBuilder.SetStap4();
-            }
-            public Product GetProduct(){
-                return objBuilder.GetProduct();
-            }
+        public override void BuildTaste()
+        {
+            coffee["taste"] = "normal";
         }
+        public override void BuildMilk()
+        {
+            coffee["milk"] = "none";
+        }
+        public override void BuildSize()
+        {
+            coffee["size"] = "normal";
+        }
+    }
+
+    //MACCHIATO
+    class MacchiatoBuilder : CoffeeBuilder
+    {
+        public MacchiatoBuilder()
+        {
+            coffee = new Coffee("Macchiato");
+        }
+        public override void BuildTaste()
+        {
+            coffee["taste"] = "soft";
+        }
+        public override void BuildMilk()
+        {
+            coffee["milk"] = "milk";
+        }
+        public override void BuildSize()
+        {
+            coffee["size"] = "large";
+        }
+    }
+
+    //RISTRETTO
+    class RistrettoBuilder : CoffeeBuilder
+    {
+        public RistrettoBuilder()
+        {
+            coffee = new Coffee("Ristretto");
+        }
+        public override void BuildTaste()
+        {
+            coffee["taste"] = "strong";
+        }
+        public override void BuildMilk()
+        {
+            coffee["milk"] = "none";
+        }
+        public override void BuildSize()
+        {
+            coffee["size"] = "small";
+        }
+    }
+
+    //MOCHA
+    class MochaBuilder : CoffeeBuilder
+    {
+        public MochaBuilder()
+        {
+            coffee = new Coffee("Mocha");
+        }
+        public override void BuildTaste()
+        {
+            coffee["taste"] = "chocolate";
+        }
+        public override void BuildMilk()
+        {
+            coffee["milk"] = "milk";
+        }
+        public override void BuildSize()
+        {
+            coffee["size"] = "large";
+        }
+    }
+
+    //IRISH
+    class IrishBuilder : CoffeeBuilder
+    {
+        public IrishBuilder()
+        {
+            coffee = new Coffee("Irish");
+        }
+        public override void BuildTaste()
+        {
+            coffee["taste"] = "whisky";
+        }
+        public override void BuildMilk()
+        {
+            coffee["milk"] = "whipped cream";
+        }
+        public override void BuildSize()
+        {
+            coffee["size"] = "large";
+        }
+    }
+
+    //FRAPPE
+    class FrappeBuilder : CoffeeBuilder
+    {
+        public FrappeBuilder()
+        {
+            coffee = new Coffee("Frappe");
+        }
+        public override void BuildTaste()
+        {
+            coffee["taste"] = "cold";
+        }
+        public override void BuildMilk()
+        {
+            coffee["milk"] = "milk and whipped cream";
+        }
+        public override void BuildSize()
+        {
+            coffee["size"] = "large";
+        }
+    }
+
+    //LATTE
+    class LatteBuilder : CoffeeBuilder
+    {
+        public LatteBuilder()
+        {
+            coffee = new Coffee("Latte");
+        }
+        public override void BuildTaste()
+        {
+            coffee["taste"] = "soft";
+        }
+        public override void BuildMilk()
+        {
+            coffee["milk"] = "milk";
+        }
+        public override void BuildSize()
+        {
+            coffee["size"] = "normal";
+        }
+    }
+
+    //GLACE
+    class GlaceBuilder : CoffeeBuilder
+    {
+        public GlaceBuilder()
+        {
+            coffee = new Coffee("Glace");
+        }
+        public override void BuildTaste()
+        {
+            coffee["taste"] = "ice cream";
+        }
+        public override void BuildMilk()
+        {
+            coffee["milk"] = "milk";
+        }
+        public override void BuildSize()
+        {
+            coffee["size"] = "large";
+        }
+    }
+
+    //AMERICANO
+    class AmericanoBuilder : CoffeeBuilder
+    {
+        public AmericanoBuilder()
+        {
+            coffee = new Coffee("Americano");
+        }
+        public override void BuildTaste()
+        {
+            coffee["taste"] = "water-ish";
+        }
+        public override void BuildMilk()
+        {
+            coffee["milk"] = "none";
+        }
+        public override void BuildSize()
+        {
+            coffee["size"] = "large";
+        }
+    }
+
+    //CAPPUCCINO
+    class CappuccinoBuilder : CoffeeBuilder
+    {
+        public CappuccinoBuilder()
+        {
+            coffee = new Coffee("Cappuccino");
+        }
+        public override void BuildTaste()
+        {
+            coffee["taste"] = "soft";
+        }
+        public override void BuildMilk()
+        {
+            coffee["milk"] = "milk";
+        }
+        public override void BuildSize()
+        {
+            coffee["size"] = "normal";
+        }
+    }
+
+    //the coffee class
+    class Coffee
+    {
+        private string _coffeeType;
+        private Dictionary<string,string,string> _parts =
+        new Dictionary<string,string,string>();
+
+        //constructor
+        public Coffee(string coffeeType)
+        {
+            this._coffeeType = coffeeType;
+        }
+
+        //indexer
+        public string this[string key]
+        {
+            get {return _parts[key];}
+            set {_parts[key] = value;}
+        }
+        public void Show()
+        {
+            Console.WriteLine("\n---------------------------");
+            Console.WriteLine("Coffee Type: {0}", _coffeeType);
+            Console.WriteLine(" Taste : {0}", _parts["taste"]);
+            Console.WriteLine(" Milk : {0}", _parts["milk"]);
+            Console.WriteLine(" Size: {0}", _parts["size"]);
+        }
+    }
 }
